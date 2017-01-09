af7adefa275a0a80794b5d0f3acd5da26aaba7df
ched <ched@FRAN0776>
2017-01-09 09:01:48 +0100

    added initial empty project structure

---
+++ Structurizr.InfrastructureAsCode.Azure/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Zuehlke Technology Group")]
+[assembly: AssemblyProduct("Structurizr.InfrastructureAsCode.Azure")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0b9571e0-151b-4eba-9e0f-1b4781a1ffdb")]
