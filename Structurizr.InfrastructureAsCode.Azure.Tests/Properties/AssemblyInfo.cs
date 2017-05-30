e18bca5dd6d2ed9747f58f0d4520be9e989b8a70
ChristianEder <christian.johann.eder@googlemail.com>
2017-05-30 11:29:06 +0200

    added test projects

---
+++ Structurizr.InfrastructureAsCode.Azure.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Zuehlke Technology Group")]
+[assembly: AssemblyProduct("Structurizr.InfrastructureAsCode.Azure.Tests")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("858898AB-D3EA-42B7-BFD6-6B173F5B79B3")]
