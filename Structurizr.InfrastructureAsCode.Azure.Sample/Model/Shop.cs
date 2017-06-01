723112ee6af7a54529ef4ea7b4bf376ee4e65508
ChristianEder <christian.johann.eder@googlemail.com>
2017-06-01 11:01:23 +0200

    added api again

--- Structurizr.InfrastructureAsCode.Azure.Sample/Model/Shop.cs
+++ Structurizr.InfrastructureAsCode.Azure.Sample/Model/Shop.cs
@@ -10,7 +10,8 @@ namespace Structurizr.InfrastructureAsCode.Azure.Sample.Model
             System = workspace.Model.AddSoftwareSystem("Shop", "");
             Customer = workspace.Model.AddPerson(Location.External, "Customer", "Buys stuff in our shop");
             Database = new ShopDatabase(this, environment);
-            Frontend = new ShopFrontend(this, Database, environment);
+            Api = new ShopApi(this, Database, environment);
+            Frontend = new ShopFrontend(this, Api, environment);
 
             Customer.Uses(Frontend.Container, "buys stuff");
             // TODO: make implicit
@@ -18,6 +19,7 @@ namespace Structurizr.InfrastructureAsCode.Azure.Sample.Model
         }
 
         public ShopFrontend Frontend { get; set; }
+        public ShopApi Api { get; set; }
         public ShopDatabase Database { get; set; }
         public Person Customer { get; set; }
     }
--- Structurizr.InfrastructureAsCode.Azure.Sample/Model/ShopFrontend.cs
+++ Structurizr.InfrastructureAsCode.Azure.Sample/Model/ShopFrontend.cs
@@ -5,21 +5,21 @@ namespace Structurizr.InfrastructureAsCode.Azure.Sample.Model
 {
     public class ShopFrontend : ContainerWithInfrastructure<AppService>
     {
-        public ShopFrontend(Shop shop, ShopDatabase database, IInfrastructureEnvironment environment)
+        public ShopFrontend(Shop shop, ShopApi api, IInfrastructureEnvironment environment)
         {
             Container = shop.System.AddContainer("Shop Frontend",
                 "Allows the user to browse and order products",
                 "ASP.NET Core MVC Web Application");
 
-            Container.Uses(database.Container, "Stores and loads products and orders");
+            Container.Uses(api.Container, "Stores and loads products and orders");
 
             Infrastructure = new AppService { Name = $"aac-sample-shop-{environment.Name}" };
 
             Infrastructure.ConnectionStrings.Add(new AppServiceConnectionString
             {
-                Name = "shop-db-uri",
+                Name = "shop-api-uri",
                 Type = "Custom",
-                Value = database.Infrastructure.Uri
+                Value = api.Infrastructure.Url
             });
         }
     }
